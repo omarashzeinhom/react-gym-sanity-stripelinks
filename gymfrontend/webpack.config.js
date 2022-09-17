1797d363155f7db5ab8ba6af8fff8e078b367430
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-18 01:09:44 +0200

    added-webpack-.conifg.js-for-multiple-pages-in-react-app

---
+++ gymfrontend/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: {
+      pageOne: './src/pages/Home/Home.jsx',
+      pageTwo: './src/pages/Contact/Contact.jsx',
+      pageThree: './src/pages/MemberShip/MemberShip.jsx',
+      pageFour: './src/pages/Classes/Classes.jsx',
+    },
+  };
